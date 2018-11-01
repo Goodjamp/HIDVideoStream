48b72bd1a11886cbb9a165d9f2bab68f6ea1efb9
Aleksandr Gerasimchuk <aleksandr.gerasimchuk@developex.org>
2018-11-01 19:58:58 +0200

    Some updates

--- Hal/HalIFlash/HalIFlash.c
+++ Hal/HalIFlash/HalIFlash.c
@@ -12,26 +12,71 @@
 
 */
 
-#define DMA_SPIF_LASH_RX_CHANEL  14
-#define DMA_SPIF_LASH_TX_CHANEL  15
+#define SPI_FLASH                 SPI7
+#define DMA_SPI_FLASH             DMA0
 
 
+#define DMA_SPIF_FLASH_RX_CHANEL  14
+#define DMA_SPIF_FLASH_TX_CHANEL  15
+
+dma_handle_t  dmaSpiFlashRxHandle;
+dma_handle_t  dmaSpiFlashTxHandle;
+
+dma_descriptor_t dmaSpiFlashRxDescriptor;
+dma_descriptor_t dmaSpiFlashTxDescriptor;
 
 void initSPIFlash(void)
 {
     DMA_Init(DMA0);
 
-    DMA_EnableChannelInterrupts(DMA0, DMA_SPIF_LASH_RX_CHANEL);
-    DMA_EnableChannelInterrupts(DMA0, DMA_SPIF_LASH_TX_CHANEL);
+    DMA_EnableChannel(DMA0, DMA_SPIF_FLASH_RX_CHANEL);
+    DMA_EnableChannel(DMA0, DMA_SPIF_FLASH_TX_CHANEL);
 
-    DMA_EnableChannel(DMA0, DMA_SPIF_LASH_RX_CHANEL);
-    DMA_EnableChannel(DMA0, DMA_SPIF_LASH_TX_CHANEL);
+    DMA_SetChannelPriority(DMA0, DMA_SPIF_FLASH_RX_CHANEL, kDMA_ChannelPriority2);
+    DMA_SetChannelPriority(DMA0, DMA_SPIF_FLASH_TX_CHANEL, kDMA_ChannelPriority2);
 
-    DMA_EnableChannelPeriphRq(DMA0, DMA_SPIF_LASH_RX_CHANEL);
-    DMA_EnableChannelPeriphRq(DMA0, DMA_SPIF_LASH_TX_CHANEL);
 }
+
+
+void sendCommand()
+
+
+void readData(uint32_t address, uint8_t *buffer, uint32_t dataSize)
+{
+    /*pull down CS*/
+    /*start send command*/
+    /*wait for completing send command*/
+    /*start receive data*/
+
+    halSpiTransferDoneCallback = callback;
+
+    DMA_CreateHandle(&masterTxHandle, SPI_DMA, EXAMPLE_SPI_MASTER_TX_CHANNEL);
+    DMA_CreateHandle(&masterRxHandle, SPI_DMA, EXAMPLE_SPI_MASTER_RX_CHANNEL);
+
+    // Set up spi master
+    SPI_MasterTransferCreateHandleDMA(EXAMPLE_SPI_MASTER, &masterHandle, SPI_MasterUserCallback, NULL, &masterTxHandle, &masterRxHandle);
+
+    // Start master transfer
+    masterXfer.txData = (uint8_t *) txData;
+    masterXfer.rxData = rxData;
+    masterXfer.dataSize = transferDataLength;
+    masterXfer.configFlags |= kSPI_FrameAssert;
+
+    return (kStatus_Success == SPI_MasterTransferDMA(EXAMPLE_SPI_MASTER, &masterHandle, &masterXfer));
+}
+
+
+
+}
+
+
+void spiFlashTransferCompliteCB(void)
+{
+
+}
+
 
 void initSPIDMAFlash(void)
 {
-
+    //
 }

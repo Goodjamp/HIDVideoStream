e17c72393543fd1b2b51f6c4d6d7b53cd9215b51
Aleksandr Gerasimchuk <aleksandr.gerasimchuk@developex.org>
2018-11-22 20:59:23 +0200

    Add cmooth to background wave, refactored logo code according code style

--- app/main.c
+++ app/main.c
@@ -18,6 +18,7 @@ int main(void)
     lcdTaskCreate();
     rtosStartScheduler();
 
+
     for (;;) {
 
     }
--- app/screenSaverProcessing/screenSaverProcessing.c
+++ app/screenSaverProcessing/screenSaverProcessing.c
@@ -131,24 +131,22 @@
 #define SET_GRAYSCALE(RB, G) (( RB << 8) | (RB << 3) | (G <<13) | (G >> 3))
 
 
-typedef uint16_t (*frameT)[SCREEN_WIDTH_PIXEL][SCREEN_HEIGHT_PIXEL];
+typedef uint16_t (*FrameBuff)[SCREEN_WIDTH_PIXEL];
 
 /********Border layer variables***************/
-typedef enum
-{
+typedef enum ColorComponents {
     RED   = 0,
     GREEN = 1,
     BLUE  = 2
-}colorT;
+} ColorComponents;
 
-typedef struct{
+typedef struct PixelDescription {
     uint8_t x;
     uint8_t y;
     uint16_t alfa;
-}pixelDescrT;
+} PixelDescription;
 
-static const uint8_t borderColor[][3] =
-{
+static const uint8_t borderColor[][3] = {
     COLOR_0,
     COLOR_1,
     COLOR_2,
@@ -158,25 +156,24 @@ static const uint8_t borderColor[][3] =
     COLOR_6,
 };
 
-struct
-{
+struct {
     float    lineKoef[3][2];
     uint32_t stopTime;
 }borderLayerDescription[COLOR_QYANTITY];
 
 //this variable should be calculate outside in future
-struct{
+struct {
     float kRB;
     float kG;
     float bRB;
     float bG;
-}wave1LayerDescription;
+} wave1LayerDescription;
 
 //this variable should be calculate outside in future
-struct{
+struct {
     uint16_t size;
-    pixelDescrT pixel[100*100];
-}border;
+    PixelDescription pixel[100*100];
+} border;
 /*******************************************/
 
 
@@ -191,26 +188,22 @@ uint16_t wave1LayerColor[128];
 uint8_t wave2LayerAlfa[128];
 
 // time section start, time section length,  position section start, section length
-const int32_t wave2TimeSectionHDesc[][5] =
-{
+const int32_t wave2TimeSectionHDesc[][5] ={
     {timePosH_0_0, timePosH_0_1, timePosH_0_1 - timePosH_0_0, posH_0_0, posH_0_1 - posH_0_0},
 };
 
 
-const int32_t wave2TimeSectionVDesc[][5] =
-{
+const int32_t wave2TimeSectionVDesc[][5] = {
     {timePosV_0_0, timePosV_0_1, timePosV_0_1 - timePosV_0_0, posV_0_0, posV_0_1 - posV_0_0},
     {timePosV_1_0, timePosV_1_1, timePosV_1_1 - timePosV_1_0, posV_1_0, posV_1_1 - posV_1_0},
 };
 
-const float wave2BizierH[2] =
-{
+const float wave2BizierH[2] = {
     waveBezierH_p1y,
     waveBezierH_p2y
 };
 
-const float wave2BizierV[2] =
-{
+const float wave2BizierV[2] = {
     waveBezierV_p1y,
     waveBezierV_p2y
 };
@@ -220,15 +213,13 @@ const float wave2BizierV[2] =
 /********Logo layer variables***************/
 extern const uint8_t image_data_logo_small[4050];
 
-const int32_t logoTimeSectionVDesc[][5] =
-{
+const int32_t logoTimeSectionVDesc[][5] = {
     {logoTimeAlfa_0_0, logoTimeAlfa_0_1, logoTimeAlfa_0_1 - logoTimeAlfa_0_0, logoAlfa_0_0, logoAlfa_0_1 - logoAlfa_0_0},
     {logoTimeAlfa_1_0, logoTimeAlfa_1_1, logoTimeAlfa_1_1 - logoTimeAlfa_1_0, logoAlfa_1_0, logoAlfa_1_1 - logoAlfa_1_0},
     {logoTimeAlfa_2_0, logoTimeAlfa_2_1, logoTimeAlfa_2_1 - logoTimeAlfa_2_0, logoAlfa_2_0, logoAlfa_2_1 - logoAlfa_2_0},
 };
 
-const float logoBizier[2] =
-{
+const float logoBizier[2] = {
     logoBezier_p1y,
     logoBezier_p2y
 };
@@ -257,14 +248,15 @@ static inline float calcCubicBezier(float t, float p0, float p1, float p2, float
 static inline int32_t calcCubicBezierPos(uint32_t cntTime, const float bezierPoints[], const int32_t timeSection[][5], const uint8_t quantitySection)
 {
     uint8_t k = 0;
-    for(;k < quantitySection; k++)
-    {
-        if(cntTime < timeSection[k][timeStop])
-        {
+
+    for(;k < quantitySection; k++) {
+        if(cntTime < timeSection[k][timeStop]) {
             break;
         }
     }
+
     float t = ((float)cntTime - (float)timeSection[k][timeStart]) / (float)timeSection[k][timeLength]; // relation time inside "k" section
+
     return (int32_t)(timeSection[k][posStart] + timeSection[k][posLength] *(3.0F*pow((1.0F-t),2.0F)*t*bezierPoints[0]+3.0F*(1.0F-t)*pow(t,2.0F)*bezierPoints[1]+pow(t,3.0F)));
 }
 
@@ -357,17 +349,18 @@ static void createBorder(void)
 }
 
 
-static void updateBorderLayer(uint32_t cntMs, uint8_t *buff)
+
+
+
+static void updateBorderLayer(uint32_t cntMs, uint8_t buff[])
 {
-    cntMs = cntMs % SCRREN_SAVER_DURATION_MS;
     uint16_t red1, green1, blue1;
     uint16_t index;
-    frameT   frame = (frameT)buff;
+    FrameBuff   frame = (FrameBuff)buff;
+    cntMs = cntMs % SCRREN_SAVER_DURATION_MS;
 
-    for(index = 0; index < COLOR_QYANTITY ; index++)
-    {
-        if(cntMs < borderLayerDescription[index].stopTime)
-        {
+    for(index = 0; index < COLOR_QYANTITY; index++) {
+        if(cntMs < borderLayerDescription[index].stopTime) {
             break;
         }
     }
@@ -382,74 +375,71 @@ static void updateBorderLayer(uint32_t cntMs, uint8_t *buff)
                                             borderLayerDescription[index].lineKoef[BLUE][1],
                                             cntMs);
 
-    for(uint16_t cnt = 0; cnt < border.size; cnt++)
-    {
+    for(uint16_t cnt = 0; cnt < border.size; cnt++) {
+        red1   = GET_RED(   frame[border.pixel[cnt].y][border.pixel[cnt].x]);
+        green1 = GET_GREEN( frame[border.pixel[cnt].y][border.pixel[cnt].x]);
+        blue1  = GET_BLUE(  frame[border.pixel[cnt].y][border.pixel[cnt].x]);
 
-        red1   = GET_RED(  (*frame)[border.pixel[cnt].y][border.pixel[cnt].x]);
-        green1 = GET_GREEN((*frame)[border.pixel[cnt].y][border.pixel[cnt].x]);
-        blue1  = GET_BLUE( (*frame)[border.pixel[cnt].y][border.pixel[cnt].x]);
+        frame[border.pixel[cnt].y][border.pixel[cnt].x] =  SET_RED(  ((255 - border.pixel[cnt].alfa)*red1   + (border.pixel[cnt].alfa)*red2  )/255);
+        frame[border.pixel[cnt].y][border.pixel[cnt].x] |= SET_GREEN(((255 - border.pixel[cnt].alfa)*green1 + (border.pixel[cnt].alfa)*green2)/255);
+        frame[border.pixel[cnt].y][border.pixel[cnt].x] |= SET_BLUE( ((255 - border.pixel[cnt].alfa)*blue1  + (border.pixel[cnt].alfa)*blue2 )/255);
 
-        (*frame)[border.pixel[cnt].y][border.pixel[cnt].x] =  SET_RED(  ((255 - border.pixel[cnt].alfa)*red1   + (border.pixel[cnt].alfa)*red2  )/255);
-        (*frame)[border.pixel[cnt].y][border.pixel[cnt].x] |= SET_GREEN(((255 - border.pixel[cnt].alfa)*green1 + (border.pixel[cnt].alfa)*green2)/255);
-        (*frame)[border.pixel[cnt].y][border.pixel[cnt].x] |= SET_BLUE( ((255 - border.pixel[cnt].alfa)*blue1  + (border.pixel[cnt].alfa)*blue2 )/255);
-
-        //(*frame)[border.pixel[cnt].y][border.pixel[cnt].x] = rezColor;
+        //frame[border.pixel[cnt].y][border.pixel[cnt].x] = rezColor;
     }
 }
 
-
-static void updateLogoLayer(uint32_t cntMs, uint8_t *buff)
+static void updateLogoLayer(uint32_t cntMs, uint8_t buff[])
 {
+    uint8_t colorRB, colorG;
+    uint16_t imageShift  = 128/2 - 45/2;
+    uint16_t (*logo)[45] = (uint16_t (*)[45])image_data_logo_small;
+    FrameBuff   frame    = (FrameBuff)buff;
+    int32_t  alfa;
+
     cntMs = cntMs % SCRREN_SAVER_DURATION_MS;
-    uint8_t colorRB;
-    uint8_t colorG;
-    uint16_t imageShift      = 128/2 - 45/2;
-    uint16_t (*logo)[45][45] =  (uint16_t (*)[45][45])image_data_logo_small;
-    frameT   frame           = (frameT)buff;
-    int32_t  alfa = calcCubicBezierPos(cntMs, logoBizier, logoTimeSectionVDesc, sizeof(logoTimeSectionVDesc) / sizeof(logoTimeSectionVDesc[0]) );
-    for(uint16_t x = imageShift, xLogo = 0; xLogo < 45; x++, xLogo++ )
-    {
-        for(uint16_t y = imageShift, yLogo = 0; yLogo < 45; y++, yLogo++ )
-        {
-             colorRB = GET_RED((*logo)[yLogo][xLogo]);
+    alfa  = calcCubicBezierPos(cntMs, logoBizier, logoTimeSectionVDesc, sizeof(logoTimeSectionVDesc) / sizeof(logoTimeSectionVDesc[0]));
+
+    for(uint16_t x = imageShift, xLogo = 0; xLogo < 45; x++, xLogo++) {
+        for(uint16_t y = imageShift, yLogo = 0; yLogo < 45; y++, yLogo++) {
+             colorRB = GET_RED(logo[yLogo][xLogo]);
              //colorG  = GET_GREEN((*logo)[yLogo][xLogo]);
              colorRB = (alfa * colorRB)/255;
              //colorG  = (alfa * colorG)/255;
 
+            frame[y][x]  = SET_GRAYSCALE(colorRB, colorRB << 1);//SET_RED(colorRB);
+            //frame[y][x] |= SET_GREEN(colorG);
+            //frame)[y][x] |= SET_BLUE(colorRB);
 
-            (*frame)[y][x]  = SET_GRAYSCALE(colorRB, colorRB << 1);//SET_RED(colorRB);
-            //(*frame)[y][x] |= SET_GREEN(colorG);
-            //(*frame)[y][x] |= SET_BLUE(colorRB);
-
-            //(*frame)[y][x] = (*logo)[yLogo][xLogo];
+            //frame[y][x] = (*logo)[yLogo][xLogo];
         }
     }
 }
 
-static void updateWave1Layer(uint32_t cntMs, uint8_t *buff)
+static void updateWave1Layer(uint32_t cntMs, uint8_t buff[])
 {
+    const uint8_t (*wavePoints)[2] = (const uint8_t (*)[2])wave;
+    uint16_t  xWave;
+    FrameBuff frame;
+    uint16_t xWaveSmooth;
+
+    frame = (FrameBuff)buff;
     cntMs = cntMs % SCRREN_SAVER_DURATION_MS;
-    const uint8_t (*wavePoints)[sizeof(wave)/2][2] =  (const uint8_t (*)[sizeof(wave)/2][2])wave;
-    frameT   frame = (frameT)buff;
-    uint16_t xWave =  1600 * calcCubicBezier((float)cntMs/(float)SCRREN_SAVER_DURATION_MS,
+    xWave =  1600 * calcCubicBezier((float)cntMs/(float)SCRREN_SAVER_DURATION_MS,
                                       waveBezier_p0y,
                                       waveBezier_p1y,
                                       waveBezier_p2y,
                                       waveBezier_p3y);
-    uint16_t xWaveSmooth = xWave;
-    for(uint16_t xFrame = waveLeftField; xFrame < waveRightField; xFrame++, xWave++)
-    {
-        if((*wavePoints)[xWave][0] > waveButtonField)
-        {
+    xWaveSmooth = xWave;
+
+    for(uint16_t xFrame = waveLeftField; xFrame < waveRightField; xFrame++, xWave++) {
+        if(wavePoints[xWave][0] > waveButtonField) {
             continue;
         }
-        if(xWave >= sizeof(wave)/2)
-        {
+        if(xWave >= sizeof(wave) / 2) {
             return;
         }
-        for(uint16_t yFrame   = (*wavePoints)[xWave][0]; yFrame < waveButtonField; yFrame++)
-        {
-            (*frame)[yFrame][xFrame] = wave1LayerColor[yFrame];
+        for(uint16_t yFrame = wavePoints[xWave][0]; yFrame < waveButtonField; yFrame++) {
+            frame[yFrame][xFrame] = wave1LayerColor[yFrame];
         }
     }
     //smoothing up of wave
@@ -457,19 +447,17 @@ static void updateWave1Layer(uint32_t cntMs, uint8_t *buff)
     uint8_t yTemp, xTemp;
     uint32_t summRB;
     uint32_t summG;
-    for(uint16_t xFrame = waveLeftField ; xFrame < waveRightField; xFrame++, xWaveSmooth++)
-    {
 
-        if((yTemp =  (*wavePoints)[xWaveSmooth][0] ) > waveButtonField)
-        {
+    for(uint16_t xFrame = waveLeftField ; xFrame < waveRightField; xFrame++, xWaveSmooth++) {
+        if((yTemp =  wavePoints[xWaveSmooth][0] ) > waveButtonField) {
             continue;
         }
         summRB = GET_RED(wave1LayerColor[yTemp]);
         summG  = GET_GREEN(wave1LayerColor[yTemp]);
-        summRB *= (*wavePoints)[xWaveSmooth][1];
+        summRB *= wavePoints[xWaveSmooth][1];
         summRB /= 10;
 
-        summG *= (*wavePoints)[xWaveSmooth][1];
+        summG *= wavePoints[xWaveSmooth][1];
         summG /= 10;
         /*
         summRB = 0;
@@ -477,112 +465,101 @@ static void updateWave1Layer(uint32_t cntMs, uint8_t *buff)
 
         //column 0
         xTemp = xFrame - 1;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         yTemp++;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         yTemp++;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         //column 1
         xTemp++;
         yTemp -= 2;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         yTemp++;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         yTemp++;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         //column 2
         xTemp++;
         yTemp -= 2;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         yTemp++;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         yTemp++;
-        summRB += GET_RED((*frame)[yTemp][xTemp]);
-        summG  += GET_GREEN((*frame)[yTemp][xTemp]);
+        summRB += GET_RED(frame[yTemp][xTemp]);
+        summG  += GET_GREEN(frame[yTemp][xTemp]);
         summRB /= 9;
         summG  /= 9;
         */
-
-        (*frame)[ (*wavePoints)[xWaveSmooth][0] - 1][xFrame] = SET_GRAYSCALE(summRB, summG);
+        frame[ wavePoints[xWaveSmooth][0] - 1][xFrame] = SET_GRAYSCALE(summRB, summG);
     }
 }
 
-
-static void updateWave2Layer(uint32_t cntMs, uint8_t *buff)
+static void updateWave2Layer(uint32_t cntMs, uint8_t buff[])
 {
-    const uint8_t (*wavePoints)[sizeof(wave)/2][2] =  (const uint8_t (*)[sizeof(wave)/2][2])wave;
-    frameT   frame  = (frameT)buff;
-    uint16_t colorRB;
-    uint16_t colorG;
-    uint32_t cntRel = (cntMs  + delayVMs) % SCRREN_SAVER_DURATION_MS;
-    int32_t  shiftY = calcCubicBezierPos(cntRel, wave2BizierV, wave2TimeSectionVDesc, sizeof(wave2TimeSectionVDesc) / sizeof(wave2TimeSectionVDesc[0]) );
-    cntRel = (cntMs  + delayHMs) % SCRREN_SAVER_DURATION_MS;
-    int32_t  shiftX = calcCubicBezierPos(cntRel, wave2BizierH, wave2TimeSectionHDesc, sizeof(wave2TimeSectionHDesc) / sizeof(wave2TimeSectionHDesc[0]) );
+    const uint8_t (*wavePoints)[2] =  (const uint8_t (*)[2])wave;
+    FrameBuff   frame  = (FrameBuff)buff;
+    uint16_t colorRB, colorG;
     int32_t yFrame;
-    for(int32_t xFrame = waveLeftField; xFrame < waveRightField; xFrame++, shiftX++ )
-    {
-        yFrame = (*wavePoints)[shiftX][0] + shiftY;
-        if(waveButtonField > waveButtonField )
-        {
+    uint32_t cntRel = (cntMs + delayVMs) % SCRREN_SAVER_DURATION_MS;
+    int32_t  shiftY = calcCubicBezierPos(cntRel, wave2BizierV, wave2TimeSectionVDesc, sizeof(wave2TimeSectionVDesc) / sizeof(wave2TimeSectionVDesc[0]));
+    cntRel = (cntMs  + delayHMs) % SCRREN_SAVER_DURATION_MS;
+    int32_t  shiftX = calcCubicBezierPos(cntRel, wave2BizierH, wave2TimeSectionHDesc, sizeof(wave2TimeSectionHDesc) / sizeof(wave2TimeSectionHDesc[0]));
+
+    for(int32_t xFrame = waveLeftField; xFrame < waveRightField; xFrame++, shiftX++) {
+        yFrame = wavePoints[shiftX][0] + shiftY;
+        if(waveButtonField > waveButtonField) {
             continue;
         }
-        if(shiftX >= sizeof(wave)/2)
-        {
+        if(shiftX >= sizeof(wave) / 2) {
             break;
         }
-        for(; yFrame < waveButtonField; yFrame++)
-        {
-             colorRB = GET_RED( (*frame)[yFrame][xFrame]);
-             colorG  = GET_GREEN((*frame)[yFrame][xFrame]);
+        for(; yFrame < waveButtonField; yFrame++) {
+             colorRB = GET_RED(  frame[yFrame][xFrame]);
+             colorG  = GET_GREEN(frame[yFrame][xFrame]);
              colorRB = ((255 - wave2LayerAlfa[yFrame]) * colorRB + wave2LayerAlfa[yFrame] * colorRBMax)/255;
-             colorG  = ((255 - wave2LayerAlfa[yFrame]) * colorG  + wave2LayerAlfa[yFrame] * colorGMax)/255;
-
-            (*frame)[yFrame][xFrame] = SET_GRAYSCALE(colorRB, colorG);
+             colorG  = ((255 - wave2LayerAlfa[yFrame]) * colorG  + wave2LayerAlfa[yFrame] * colorGMax) /255;
+             frame[yFrame][xFrame] = SET_GRAYSCALE(colorRB, colorG);
         }
     }
 }
-
 
 static void initWave1Layer(void)
 {
     float   kRB, kG;
     float   bRB, bG;
     uint8_t colorRB;
-    uint8_t colorG;;
+    uint8_t colorG;
     calcLineKoef(&kRB, &bRB,
                  34,   RED_TO_565(53),
                  122,  RED_TO_565(27));
     calcLineKoef(&kG, &bG,
                  34,   GREEN_TO_565(53),
                  122,  GREEN_TO_565(27));
-    for(uint8_t y = 0; y < sizeof(wave1LayerColor)/sizeof(wave1LayerColor[0]); y++ )
-    {
+    for(uint8_t y = 0; y < sizeof(wave1LayerColor)/sizeof(wave1LayerColor[0]); y++) {
         colorRB = calcLinePoint(kRB, bRB, y);
         colorG  = calcLinePoint(kG, bG, y);
         wave1LayerColor[y]  = SET_RED(colorRB);
         wave1LayerColor[y] |= SET_GREEN(colorG);
         wave1LayerColor[y] |= SET_BLUE(colorRB);
     }
-
 }
 
 static void initWave2Layer(void)
 {
-    float   k;
-    float   b;
+    float k;
+    float b;
     calcLineKoef(&k, &b,
                  0,   150,
                  127, 20);
-    for(uint8_t y = 0; y < sizeof(wave2LayerAlfa)/sizeof(wave2LayerAlfa[0]); y++ )
-    {
+    for(uint8_t y = 0; y < sizeof(wave2LayerAlfa)/sizeof(wave2LayerAlfa[0]); y++ ) {
         wave2LayerAlfa[y] = calcLinePoint(k, b, y);
     }
 }
@@ -591,8 +568,7 @@ static void initBorderLayer(void)
 {
     uint16_t colorTimeStep = SCRREN_SAVER_DURATION_MS / COLOR_QYANTITY;
     uint16_t colorStopTime = 0;
-    for(uint8_t cnt = 0; cnt < COLOR_QYANTITY ; cnt++ )
-    {
+    for(uint8_t cnt = 0; cnt < COLOR_QYANTITY ; cnt++ ) {
         borderLayerDescription[cnt].stopTime = (cnt == (COLOR_QYANTITY - 1)) ? (SCRREN_SAVER_DURATION_MS) : (colorStopTime + colorTimeStep);
         // calculation line coefficient for interpolation red color
         calcLineKoef(&borderLayerDescription[cnt].lineKoef[RED][0],
